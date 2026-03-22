6ff22692ec1d4cc5bccec44d852b2f70f092d210
JinaNavo <it22070012@my.sliit.lk>
2026-03-22 11:32:55 +0530

    Created swagger

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+const YAML = require("yamljs");
+const swaggerUi = require("swagger-ui-express");
 const { corsOptions } = require("./config/cors");
 
 const app = express();
@@ -8,6 +11,9 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use("/api/products", require("./routes/productRoutes"));
+
+const swaggerDocument = YAML.load(path.join(__dirname, "..", "openapi.yaml"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
 	res.send("Product Service Running");
